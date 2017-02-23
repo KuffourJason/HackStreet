928b675e15a2dda3144eb2528b5dfff2db7d4af4
Jason <jasfour.15@hotmail.com>
2017-02-22 23:06:02 -0500

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(process.env.PORT || 1337, function () {
+var server = app.listen(process.env.PORT || 1337, "localhost", function () {
 
   var host = server.address().address
   var port = server.address().port
