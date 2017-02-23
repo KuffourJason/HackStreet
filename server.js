25d2355e7b70fc2136ef609621b117f14f1581d9
Jason <jasfour.15@hotmail.com>
2017-02-23 00:22:15 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var http = require('http');
 var express = require('express');
+var Connection = require('tedious').Connection; 
 
 var app = express();
 app.listen(process.env.PORT || 8000);
+
+/*
+var config = {  
+	userName: 'yourusername',  
+        password: 'yourpassword',  
+        server: 'yourserver.database.windows.net',  
+        // If you are on Microsoft Azure, you need this:  
+        options: {encrypt: true, database: 'AdventureWorks'}  
+};  
+
+var connection = new Connection(config);  
+
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.  
+        console.log("Connected");  
+    });
+*/
 
 app.get('/', function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
