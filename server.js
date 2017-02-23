32a78a420788044ea012cf92e3b79d6f72c9f29a
Jason <jasfour.15@hotmail.com>
2017-02-23 00:09:53 -0500

    included package.json

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http')
+var http = require('http');
+var express = require('express');
 
- var port = process.env.PORT || 1337;
- 
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello World\n');
- }).listen(port);
+var app = express();
+app.listen(process.env.PORT || 8000);
+
+app.get('/', function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end( "home page" );
+})
+
+//query example
+app.get('/listUsers', function (req, res) {
+
+})
+
+//query the database here
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   
+})
